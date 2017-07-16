bd7275306e0f94311c5b9cc658e9ae36a1c946ad
Pattarawan Sriprasert <pattarawan@Macintosh.local>
2017-07-16 19:29:43 +0700

    Testing HTTP Request from google map APIs

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ request({
     url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longtitude: ${body.results[0].geometry.location.lng}`);
 });
