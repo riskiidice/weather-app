e6083babb15077955e0178f337e7f79d084eec28
Pattarawan Sriprasert <pattarawan@Macintosh.local>
2017-07-16 20:27:10 +0700

    Add encodeURIComponent

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs.options({
+    a:{
+        demand: true,
+        alias: 'address',
+        describe: 'Addressto fetch weather for',
+        string: true
+    }
+})
+.help()
+.alias('help','h')
+.argv;
+// Raw Address : 1301 lombard street philadelphia
+
+var encodeAddress = encodeURIComponent(argv.address)
 
 request({
-    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}`,
     json: true
 }, (error, response, body) => {
     console.log(`Address: ${body.results[0].formatted_address}`);
