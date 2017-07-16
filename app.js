4e84b339ff70e3f45f618fc02f197374ff2b711c
Pattarawan Sriprasert <pattarawan@Macintosh.local>
2017-07-16 22:18:33 +0700

    Add forecast APIs

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const yargs = require('yargs');
+/* const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode')
 
@@ -21,3 +21,22 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
         console.log(JSON.stringify(results, undefined, 2));
     }
 });
+ */
+//037637312525a1e5a6a240d01bc977fe
+const request = require('request');
+
+request({
+    url: `https://api.darksky.net/forecast/037637312525a1e5a6a240d01bc977fe/13.858813,100.4875818`,
+    json: true
+}, (error, response, body) => {
+
+    if (error) {
+        console.log('Unable to connect forecast.io server');
+    } else if (response.statusCode === 400) {
+        console.log('Unable to fetch weather');
+    } else if (response.statusCode === 200) {
+        console.log(body.currently.temperature);
+    }
+
+}
+)
