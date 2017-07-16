bc1f9335b1af97307d7a7e0875d028e3ad93ffee
Pattarawan Sriprasert <pattarawan@Macintosh.local>
2017-07-16 20:34:10 +0700

    Add callback error inspection

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const request = require('request');
 const yargs = require('yargs');
 
 const argv = yargs.options({
-    a:{
+    a: {
         demand: true,
         alias: 'address',
         describe: 'Addressto fetch weather for',
         string: true
     }
 })
-.help()
-.alias('help','h')
-.argv;
+    .help()
+    .alias('help', 'h')
+    .argv;
 // Raw Address : 1301 lombard street philadelphia
 
 var encodeAddress = encodeURIComponent(argv.address)
@@ -20,7 +20,13 @@ request({
     url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}`,
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longtitude: ${body.results[0].geometry.location.lng}`);
+    if (error) {
+        console.log('Cannot connect to Google Server');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find address');
+    } else if (body.status === "OK") {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longtitude: ${body.results[0].geometry.location.lng}`);
+    }
 });
